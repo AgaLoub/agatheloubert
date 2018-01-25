4aebefc8f11dd4a6a87bd46349de7dee97b73faa
Agathe Loubert <agaloubert@hotmail.com>
2018-01-25 16:32:52 +0100

    changes done

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,14 @@
+// import swal from 'sweetalert';
+
+// function bindSweetAlertButtonDemo() {
+//   document.getElementById('sweet-alert-demo').addEventListener('click', () => {
+//     swal({
+//       title: "Merci pour votre message",
+//       text: "Je reviendrai vers vous dans les plus brefs délais",
+//       icon: "success"
+//     })
+//   });
+// }
+
+// export { bindSweetAlertButtonDemo };
+
