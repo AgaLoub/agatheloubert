c6f9b8a1a13e160f6777493e6752d51aaf100ca8
Agathe Loubert <agaloubert@hotmail.com>
2018-01-25 17:25:14 +0100

    pb de scroll résolu

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -1,7 +1,7 @@
-require 'test_helper'
+# require 'test_helper'
 
-class ContactsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
+# class ContactsControllerTest < ActionDispatch::IntegrationTest
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
