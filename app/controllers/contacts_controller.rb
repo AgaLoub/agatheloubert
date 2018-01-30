e9b3b31daa4391ec360b4925456c5889971eb695
Agathe Loubert <agaloubert@hotmail.com>
2018-01-30 22:28:45 +0100

    heroku

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash.now[:error] = nil
-      redirect_to root_path, notice: "Je vous remercie pour votre message, je reviendrais vers vous dans les plus brefs délais"
+      redirect_to root_path, notice: "Je vous remercie pour votre message, je reviendrai vers vous dans les plus brefs délais"
     else
       flash.now[:error] = "Le message n'a pas pu être envoyé"
       redirect_to root_path, alert: "Le message n'a pas pu être envoyé, veuillez réessayer"
