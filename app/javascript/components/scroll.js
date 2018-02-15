228bba45717d087847a7eeb1044780a124ef9944
Agathe Loubert <agaloubert@hotmail.com>
2018-02-15 16:13:06 +0100

    changes done

--- app/javascript/components/scroll.js
+++ app/javascript/components/scroll.js
@@ -1,4 +1,5 @@
 const links = document.querySelectorAll('.navbar-wagon-link');
+const buton = document.querySelectorAll('.coordonnees');
 
 // const sections = document.querySelectorAll('.section');
 // sections.forEach(section => {
@@ -15,10 +16,11 @@ function handleClick(event) {
     document.querySelector('#projets').scrollIntoView({ behavior: "smooth", block: "start"});
   } else if (event.target.classList.contains('contact')) {
     document.querySelector('#contact').scrollIntoView({ behavior: "smooth", block: "start"});
+  } else if (event.target.classList.contains('coordonnees')) {
+    document.querySelector('#contact').scrollIntoView({ behavior: "smooth", block: "start"});
   }
 }
 
-
-
-
 links.forEach(link => link.addEventListener('click', handleClick))
+buton.forEach(buton => buton.addEventListener('click', handleClick))
+
